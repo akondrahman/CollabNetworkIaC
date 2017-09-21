ca6e8f5472405c2cdde67aa7acc4b76df46d1e27
Akond Rahman <akond.rahman.buet@gmail.com>
2017-09-21 19:44:29 -0400

    IaC Graph: code extraction

--- graph-extract/extract_main.py
+++ graph-extract/extract_main.py
@@ -11,7 +11,16 @@ if __name__=='__main__':
    # dataset_file   = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Mozilla.Final.Categ.csv'
    # dataset_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Openstack.Final.Categ.csv'
    dataset_file = '/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Defect-Categ-Project/output/Wikimedia.Final.Categ.csv'
+   threshold    = 10
 
-   all_months_in_ds, file_per_mon_dict = graph_utils.getAllMonthsFromDataset(dataset_file)
+   all_months_in_ds, file_per_mon_dict, file_defect_dict, repo_dict = graph_utils.getAllMonthsFromDataset(dataset_file)
    for mon_, file_per_mon in file_per_mon_dict.iteritems():
-       print 'Month:{}, file:{}'.format(mon_, len(file_per_mon))
+       #print 'Month:{}, file:{}'.format(mon_, len(file_per_mon))
+       if (len(file_per_mon)>= threshold):
+          for each_file in file_per_mon:
+              defect_status = file_defect_dict[each_file]
+              repo_of_file  = repo_dict[each_file]
+
+              prog_names=graph_utils.getUniqueDevs(each_file, repo_of_file)
+              print 'File:{}, repo:{}, defect:{}, programmers:{}'.format(each_file, repo_of_file, defect_status, prog_names)
+              print '-'*100
--- graph-extract/graph_utils.py
+++ graph-extract/graph_utils.py
@@ -3,6 +3,9 @@ graph utility file for Sep 21, 2017
 Thursday
 Akond Rahman
 '''
+import os, csv, numpy as np
+import pandas as pd
+import subprocess
 
 def getYear(single_date):
     dt2ret = single_date.split('-')[0]
@@ -27,7 +30,7 @@ def getTimeInfo(id_param, repo_param):
              dict2see[id_] = ts_
     return dict2see[id_param]
 
-def getMonthFileCount(df_param, mf, yf):
+def getMonthFileCount(df_param):
     _df = pd.DataFrame(df_param, columns=['ID', 'REPO', 'FILE', 'TIME', 'DEFECTSTATUS'])
     _df['YEAR']  = _df['TIME'].apply(getYear)
     _df['MONTH'] = _df['TIME'].apply(getMonth)
@@ -45,7 +48,7 @@ def getAllMonthsFromDataset(categ_file_param):
        '''
        dicionary to hold files for each month
        '''
-       month_file_dict = {}
+       month_file_dict, defect_file_dict, repo_file_dict = {}, {}, {}
        with open(categ_file_param, 'rU') as f:
          reader_ = csv.reader(f)
          next(reader_, None)
@@ -58,7 +61,7 @@ def getAllMonthsFromDataset(categ_file_param):
              else:
                  defect_status = '0'
              filepath_ = row[4]
-             time_ = graph_utils.getTimeInfo(id_, repo_)
+             time_ = getTimeInfo(id_, repo_)
              time2write = time_.split(' ')[0]
              df_list.append((id_, repo_, filepath_, time2write, defect_status))
              '''
@@ -69,6 +72,42 @@ def getAllMonthsFromDataset(categ_file_param):
                  month_file_dict[month2write] = [filepath_]
              else:
                  month_file_dict[month2write] = month_file_dict[month2write] + [filepath_]
+             '''
+             dictionary files
+             '''
+             if filepath_ not in defect_file_dict:
+                 defect_file_dict[filepath_] = defect_status
+             '''
+             dictionary repos
+             '''
+             if filepath_ not in repo_file_dict:
+                 repo_file_dict[filepath_] = repo_
        # get all months, and files in the month for the dataset
-       m_list, m_c_list = graph_utils.getMonthFileCount()
-       return m_list, month_file_dict
+       m_list, m_c_list = getMonthFileCount(df_list)
+       return m_list, month_file_dict, defect_file_dict, repo_file_dict
+
+
+
+def getUniqueDevs(param_file_path, repo_path):
+
+   cdCommand         = "cd " + repo_path + " ; "
+   theFile           = os.path.relpath(param_file_path, repo_path)
+   #print "full path: {}, repo path:{}, theFile:{}".format(param_file_path, repo_path, theFile)
+   commitCountCmd    = " git blame "+ theFile +"  | awk '{print $2}' | cut -d'(' -f2 "
+   command2Run = cdCommand + commitCountCmd
+
+   commit_count_output = subprocess.check_output(['bash','-c', command2Run])
+   author_count_output = commit_count_output.split('\n')
+   author_count_output = [x_ for x_ in author_count_output if x_!='']
+
+   return author_count_output
+
+'''
+Graph construction zone
+'''
+def getEdges(uni_prog_names):
+    
+
+def constructGraph(prog_list_param):
+    nodes = np.unique(prog_list_param)
+    edges = getEdges(nodes)
