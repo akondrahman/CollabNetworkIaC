07ff8e7b5bd5e7bbb192ed9f0d32fcecd6265592
Akond Rahman <akond.rahman.buet@gmail.com>
2017-09-22 12:14:24 -0400

    extraction code: metrics

--- graph-extract/graph_metric_extraction.R
+++ graph-extract/graph_metric_extraction.R
@@ -41,7 +41,9 @@ for(dir2search in dirs2search)
     
     edge_dens_vec  <- c()
     clus_coeff_vec <- c()                      
-    between_vec    <- c()    
+    
+    med_bet_vec <- c()
+    avg_bet_vec <- c()
     
     med_close_vec <- c()
     avg_close_vec <- c()              
@@ -100,12 +102,19 @@ for(dir2search in dirs2search)
 
       edg_dens     <- edge_density(net, loops=T)
       clust_coeff  <- transitivity(net)
-      edge_betw    <- edge.betweenness(net, e=E(net))
+
       
       edge_dens_vec  <- append(edge_dens_vec, edg_dens)
       clus_coeff_vec <- append(clus_coeff_vec, clust_coeff)                      
-      between_vec    <- append(between_vec, edge_betw)       
 
+      edge_betw    <- edge.betweenness(net, e=E(net))
+      med_e_bet    <- median(edge_betw)
+      avg_e_bet    <- mean(edge_betw)
+
+      med_bet_vec  <- append(med_bet_vec, med_e_bet)       
+      avg_bet_vec  <- append(avg_bet_vec, avg_e_bet)       
+
+      
       closeness   <- closeness(net, vids=V(net))
       avg_close   <- mean(closeness)
       med_close   <- median(closeness)
@@ -118,13 +127,34 @@ for(dir2search in dirs2search)
       modularity_vec <- append(modularity_vec, modula_)    
       
     }
+#     print(length(file_vec))
+#     print(length(med_in_deg_vec))
+#     print(length(avg_in_deg_vec))
+#     print(length(med_out_deg_vec))
+#     print(length(avg_out_deg_vec))    
+#     print(length(med_all_deg_vec))
+#     print(length(avg_all_deg_vec))    
+#     print(length(assort_vec))
+#     print(length(dia_vec))
+#     print(length(avg_ecce_vec))
+#     print(length(edge_dens_vec))
+#     print(length(clus_coeff_vec))    
+#     print(length(med_bet_vec))
+#     print(length(avg_bet_vec))
+#     print(length(med_close_vec))    
+#     print(length(avg_close_vec))    
+#     print(length(modularity_vec))
+#     print(length(defect_vec))    
+    
     #### NOW APPEND !!! 
+    print(dir2search)
     graph.metric.data <- data.frame(file_vec, med_in_deg_vec, avg_in_deg_vec, 
                                               med_out_deg_vec, avg_out_deg_vec, 
                                               med_all_deg_vec, avg_all_deg_vec, 
                                               assort_vec, dia_vec, 
                                               med_ecce_vec, avg_ecce_vec,
-                                              edge_dens_vec, clus_coeff_vec, between_vec, 
+                                              edge_dens_vec, clus_coeff_vec,
+                                              med_bet_vec, avg_bet_vec, 
                                               med_close_vec, avg_close_vec, modularity_vec, 
                                               defect_vec)
     print(head(graph.metric.data))
@@ -138,71 +168,3 @@ for(dir2search in dirs2search)
 t2 <- Sys.time()
 print(t2 - t1)  
 rm(list = setdiff(ls(), lsf.str()))
-
-# transitivity(test.graph,type="local")
-# # gives the clustering coefficient of each node
-
-#       print(degree_)
-#       V(net)$size <- degree_ * 2
-#       
-#       # Set edge width based on weight:
-#       E(net)$width <- E(net)$weight/6
-#       
-#       #change arrow size and edge color:
-#       E(net)$arrow.size <- .2
-#       E(net)$edge.color <- "blue"
-#       E(net)$width <- 1 + E(net)$weight / 12
-#       
-#       # plot(net, edge.arrow.size=.5,  edge.curved=.1, vertex.shape="square" )
-
-
-# only_file_name=basename(file2search)
-# if(substr(only_file_name, start=1, stop=4)=='node')
-# {
-#   node_file_vec <- append(node_file_vec, file2search)
-# }
-# if(substr(only_file_name, start=1, stop=4)=='edge')
-# {
-#   print(file2search)
-#   edge_file_vec <- append(edge_file_vec, file2search)
-# }
-
-
-
-# for(filename_ in filenames)
-# {
-#   #TODO read from file : get node file name and get edge file name 
-#   
-#   nodes <- read.csv("/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Graph/dataset/WIKIMEDIA/2012-10/node.Users.akond.PUPP_REPOS.wikimedia-downloads.cdh4.manifests.hadoop.pp.csv", header=T, as.is=T)
-#   edges <- read.csv("/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Graph/dataset/WIKIMEDIA/2012-10/edge.Users.akond.PUPP_REPOS.wikimedia-downloads.cdh4.manifests.hadoop.pp.csv", header=T, as.is=T)
-#   
-#   net <- graph_from_data_frame(d=edges, vertices=nodes, directed=T) 
-#   
-#   # # Compute node degrees (#links) and use that to set node size:
-#   print("Degree ...")
-#   degree_ <- degree(net, mode="all")
-#   degree_
-#   V(net)$size <- degree_ * 2
-#   
-#   # Set edge width based on weight:
-#   E(net)$width <- E(net)$weight/6
-#   
-#   #change arrow size and edge color:
-#   E(net)$arrow.size <- .2
-#   E(net)$edge.color <- "blue"
-#   E(net)$width <- 1 + E(net)$weight / 12
-#   
-#   # plot(net, edge.arrow.size=.5,  edge.curved=.1, vertex.shape="square" )
-#   
-#   print("Assortivity")
-#   print(assortativity_degree(net))
-#   print("Diameter")
-#   print(diameter(net, directed=F))
-#   print("Eccentricity")
-#   print(eccentricity(net))
-#   print("Page rank")
-#   page_rank(net)$vector
-#   print("Edge density")
-#   edge_density(net, loops=T)
-#   
-# }
