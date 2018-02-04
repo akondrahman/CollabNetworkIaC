9a11e24c2a3953bcff851f7e60afb94259ae4675
Akond Rahman <akond.rahman.buet@gmail.com>
2018-02-04 16:18:24 -0500

    meenely dev metrics

--- graph-extract/main_meenely_dev.py
+++ graph-extract/main_meenely_dev.py
@@ -3,6 +3,48 @@ Akond Rahman
 Feb 04, 2018
 Meenely's Dev Network
 '''
+import os
+import csv
+import itertools
+
+def getGitProgrammerInfo(param_file_path, repo_path):
+   cdCommand         = "cd " + repo_path + " ; "
+   theFile           = os.path.relpath(param_file_path, repo_path)
+   commitCountCmd    = " git blame "+ theFile +"  | awk '{print $2}' | cut -d'(' -f2 "
+   command2Run = cdCommand + commitCountCmd
+
+   commit_count_output = subprocess.check_output(['bash','-c', command2Run])
+   author_count_output = commit_count_output.split('\n')
+   author_count_output = [x_ for x_ in author_count_output if x_!='']
+   node_cnt            = len(np.unique(author_count_output)) ## get node count
+   ## create edges using combinations
+   edge_list = list(itertools.permutations(author_count_output, 2))
+   return (edge_list, node_cnt)
+
+def getGraphData(file_path_p, repo_path_p):
+    if 'moz' in file_path_p:
+        graph_per_file = getHgProgrammerInfo(file_path_p, repo_path_p)
+    else:
+        graph_per_file = getGitProgrammerInfo(file_path_p, repo_path_p)
+    ### now return value
+    print 'File:{}, Graph:{}'.format(file_path_p, graph_per_file)
+    # print 'Processing {} ...'.format(file_path_p)
+    return graph_per_file
+
+def getGraphForFiles(file_path_p):
+    output_dict = {}
+    with open(file_path_p, 'rU') as file_:
+      reader_ = csv.reader(file_)
+      next(reader_, None)
+      for row_ in reader_:
+        repo_of_file       = row_[1]
+        categ_of_file      = row_[3]
+        full_path_of_file  = row_[4]
+        if os.path.exists(full_path_of_file):
+            if full_path_of_file not in output_dict:
+                gr_for_file = getGraphData(full_path_of_file, repo_of_file)
+                output_dict[full_path_of_file] = gr_for_file
+    return output_dict
 
 if __name__=='__main__':
     ### INPUT
@@ -21,5 +63,5 @@ if __name__=='__main__':
     # datasetFile2Save='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Graph/dev-ds/OST.PKL'
     # datasetFile2Save='/Users/akond/Documents/AkondOneDrive/OneDrive/IaC-Graph/dev-ds/WIK.PKL'
 
-    final_dict = getEdgeForFiles(theCompleteCategFile)
+    final_dict = getGraphForFiles(theCompleteCategFile)
     # pickle.dump(final_dict, open(datasetFile2Save, 'wb'))
